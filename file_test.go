1b1d4c01bf74459355bc295b0da21420f7c4e2e1
0xpeIpeI <hokumaru12@gmail.com>
2023-06-04 16:28:07 +0900

    delete test

--- file_test.go
+++ file_test.go
@@ -1,29 +1,11 @@
 package g3drive_test
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/lll-lll-lll/g3drive"
 	"github.com/stretchr/testify/assert"
 )
-
-func Test_ReadDir(t *testing.T) {
-	t.Parallel()
-	file, err := os.Open("a/file1.txt") // For read access.
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	data, err := io.ReadAll(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(data))
-}
 
 func Test_Parse_PDF(t *testing.T) {
 	t.Parallel()
